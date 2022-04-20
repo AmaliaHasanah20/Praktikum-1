64d7f69b229ce123033a8a4a3ce053fd8be8b291
bintusaad_ <91559351+AmaliaHasanah20@users.noreply.github.com>
2022-04-20 15:11:51 +0700

    Add files via upload

---
+++ praktikum-1/variable_user.php
@@ -0,0 +1,14 @@
+<?php 
+//mendefinisikan variable
+    $nama = "Amalia Hasanah";
+    $umur = 20;
+    $berat = 55;
+//Mencetak variable
+    echo "nama :". $nama;
+    echo "<br/>umur :". $umur. "Tahun";
+    echo "<br/>berat :". $berat. "Kg";
+
+    echo "<br/>Hello! $nama apa kabar?"; 
+
+
+?>
